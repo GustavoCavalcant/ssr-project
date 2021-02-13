9e8ac3e97295ac342c255b5826821c1c95d7b289
Gustavo Cavalcante Ferreira <gustavo2402.00@gmail.com>
2021-02-12 23:22:38 -0300

    projeto de entrega SSR

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Button from './components/Button';
+
+function HomePage() {
+    return (
+    <div>
+        SSR - Projeto React Back!
+        <Button />
+    </div>
+    );
+}
+  
+  export default HomePage
